ecf9db20e3c4da7f8098a03fc10d0739a3894394
LucasOliver <78925489+spinho010@users.noreply.github.com>
2021-12-14 09:46:36 -0300

    enxugando o projeto

--- projeto/core/admin.py
+++ projeto/core/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from .models import ListaProdutos
 from django.contrib.auth import admin as auth_admin
 # Register your models here.
 
-class ListaProdutosAdmin(admin.ModelAdmin):
-    list_display = ('nome_produto', 'descricao_produto', 'marca_produto', 'custo_produto', 'preço_produto')
-
-admin.site.register(ListaProdutos, ListaProdutosAdmin)
--- projeto/core/views.py
+++ projeto/core/views.py
@@ -3,19 +3,6 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 from django.http import HttpResponse
-from .models import ListaProdutos
 
 def index(request):
     return render(request, 'index.html')
-
-
-class ListLista(ListView):
-    model = ListaProdutos
-    template_name = 'listaproducts.html'
-
-
-class CreateLista(CreateView):
-    model = ListaProdutos
-    fields = ['nome_produto', 'descricao_produto', 'marca_produto', 'custo_produto', 'preço_produto']
-    template_name = 'createproducts.html'
-    success_url = ('/')
